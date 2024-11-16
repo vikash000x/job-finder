c14542a5030e2c093fb354495ba02d11fffb1189
vikashsinha2603 <vikashsinha045@gmail.com>
2024-11-16 15:07:06 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://job-finder-kzmj.onrender.com',
     credentials:true
 }
 
